d7c1e64ff2ba005085659ec5c1526699a995218c
Ossie Finnegan <Ossie.finnegan.sf@gmail.com>
2025-11-21 19:20:32 -0800

    Enhance comments with links to LeetCode problem
    Added links to problem and submission for reference.

--- arrays/easy/contains_duplicate.py
+++ arrays/easy/contains_duplicate.py
@@ -1,6 +1,8 @@
+# Link: https://leetcode.com/problems/contains-duplicate
 class Solution:
 
-  # Initial solution
+  # Initial solution https://leetcode.com/problems/contains-duplicate/submissions/1836400946/
+
   def containsDuplicate(self, nums: List[int]) -> bool:
     from collections import Counter
 
@@ -10,6 +12,7 @@ class Solution:
       return True
     return False
 
-    # After learning that sets remove duplicates, we can distill down to:
+    # After learning that sets remove duplicates, we can distill down
+    # https://leetcode.com/problems/contains-duplicate/submissions/1836403504/
     def containsDuplicateOptimal(self, nums: List[int]) -> bool:
       return len(nums) != len(set(nums))
