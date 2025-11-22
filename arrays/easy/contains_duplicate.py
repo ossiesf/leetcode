7b568e163f4c525f1bb1fe3fac993d0677fd84c8
Ossie Finnegan <Ossie.finnegan.sf@gmail.com>
2025-11-21 19:24:18 -0800

    Optimize containsDuplicate method using set
    Refactor containsDuplicate method to use set for optimal solution.

--- arrays/easy/contains_duplicate.py
+++ arrays/easy/contains_duplicate.py
@@ -1,4 +1,6 @@
 # Link: https://leetcode.com/problems/contains-duplicate
+# Initial solution - O(n) time, O(n) space
+# Optimal solution - O(n) time, O(n) space
 class Solution:
 
   # Initial solution https://leetcode.com/problems/contains-duplicate/submissions/1836400946/
@@ -13,6 +15,7 @@ class Solution:
     return False
 
     # After learning that sets remove duplicates, we can distill down
+    # Simpler and faster - no counting overhead
     # https://leetcode.com/problems/contains-duplicate/submissions/1836403504/
     def containsDuplicateOptimal(self, nums: List[int]) -> bool:
       return len(nums) != len(set(nums))
