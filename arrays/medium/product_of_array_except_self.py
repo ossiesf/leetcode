7d6b6fdd5ea46a31516f032b1e34950786955664
Ossie Finnegan <Ossie.finnegan.sf@gmail.com>
2026-01-01 13:18:50 -0800

    Add efficient solution for product of array except self
    Implemented an efficient O(n) solution using prefix and suffix arrays to calculate the product of array elements except self.

--- arrays/medium/product_of_array_except_self.py
+++ arrays/medium/product_of_array_except_self.py
@@ -1,3 +1,32 @@
+# Second solution, O(n)
+# Link: https://leetcode.com/problems/product-of-array-except-self/submissions/1871407523/
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Prefix and suffix approach
+        # First calculate for each index 'product of everything before me'
+        # Then calc 'product of everything after me'
+        # Then multiply the results
+        size = len(nums)
+
+        prefix = [0] * size
+        running_total = 1
+        for i in range(size):
+            # Save current total before creating new product
+            # As to exclude the current index in the calculation
+            prefix[i] = running_total
+            running_total = nums[i] * running_total
+
+        suffix = [0] * size
+        running_total = 1
+        for i in range(size - 1, -1, -1):
+            suffix[i] = running_total
+            running_total = nums[i] * running_total
+
+        for i in range(size):
+            nums[i] = prefix[i] * suffix[i]
+
+        return nums
+
 # This is not an efficient solution, O(n^2)
 # Results in a timeout if nums is too large
 # TODO: New solution with complexity < n^2
