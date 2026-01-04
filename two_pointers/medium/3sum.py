5af4a77aebaec4684c1cec34a86c3ee83a2e6d43
Ossie Finnegan <Ossie.finnegan.sf@gmail.com>
2026-01-03 20:52:21 -0800

    Improve threeSum solution with duplicate checks
    Refactor threeSum method to handle duplicates and improve efficiency.

--- two_pointers/medium/3sum.py
+++ two_pointers/medium/3sum.py
@@ -1,5 +1,47 @@
-# Initial draft solution, does not pass all test cases yet
 # Link: https://leetcode.com/problems/3sum/
+# Second solution, this one with checks for dupes and avoid checking
+# If 'triplets' in result as this is expensive
+# See comments for changes
+# Solution is O(n^2)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        size = len(nums)
+        k = size-1
+        result = []
+
+        nums = sorted(nums)
+
+        for i in range(size-2):
+            # Check for dupes of i
+            # If dupe, go to next iteration of for loop
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            j = i+1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total > 0:
+                    k-=1
+                elif total < 0:
+                    j+=1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+
+                    # Check / skip duplicates for j & k
+                    while j < k and nums[j] == nums[j+1]:
+                        j+=1
+                    while j < k and nums[k] == nums[k-1]:
+                        k-=1
+
+                    # Since the list is sorted, if we find a match
+                    # Increment j: we are gauranteed to be > 0 for all other values of j
+                    # Decrement k to balance that out
+                    j+=1
+                    k-=1
+            k = size-1
+        return result
+
+# Initial draft solution, does not pass all test cases yet
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         size = len(nums)
